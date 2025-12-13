5b9a0f2f5530c2cc39ebbb7b4412f16e6799fa78
Daniel Ayodeji Obamiyi <obamiyiayodeji@gmail.com>
2025-12-13 10:54:12 +0100

    feat(footer): complete footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Card from './pages/Card/Card'
-import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
- 
- const App = () => {
-   return (
-     <div className='app'>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/card' element={<Card/>}/>
-        <Route path='/placeorder' element={<PlaceOrder/>}/>
-      </Routes>
-     </div>
-   )
- }
- 
- export default App
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Card from "./pages/Card/Card";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+import Footer from "./components/Footer/Footer";
+
+const App = () => {
+  return (
+    <>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/card" element={<Card />} />
+          <Route path="/placeorder" element={<PlaceOrder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
