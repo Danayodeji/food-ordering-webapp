11728b18c852697ddc0c0fefcd3d13ecd71528c1
Daniel Ayodeji Obamiyi <obamiyiayodeji@gmail.com>
2025-12-14 09:13:32 +0100

    (feature: completed the home page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home/Home";
 import Card from "./pages/Card/Card";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import Footer from "./components/Footer/Footer";
+import AppDownload from "./components/AppDownload/AppDownload";
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
           <Route path="/placeorder" element={<PlaceOrder />} />
         </Routes>
       </div>
+      <AppDownload />
       <Footer />
     </>
   );
